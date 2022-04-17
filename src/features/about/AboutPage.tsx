a90216ad64ab16da281335574acf0a4a4ac43dad
Aybar Taş <aybartass@gmail.com>
2022-04-17 21:06:19 +0300

    toast notifications & api later added

--- src/features/about/AboutPage.tsx
+++ src/features/about/AboutPage.tsx
@@ -1,6 +1,19 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import { apiAgent } from "../../app/api/ApiService";
 
 export default function AboutPage (){
-    console.log("about")
-    return (<Typography>About </Typography>);
+    console.log("about");
+
+    return (
+        <Container>
+            <Typography> Testing purposes only</Typography>
+            <ButtonGroup>
+                <Button onClick={apiAgent.TestErrors.getBadRequest}> badrequest</Button>
+                <Button onClick={apiAgent.TestErrors.getNotFound}> notfound</Button>
+                <Button onClick={apiAgent.TestErrors.getServerError} >server</Button>
+                <Button onClick={apiAgent.TestErrors.getUnauthorized}>unauthorized </Button>
+                <Button onClick={apiAgent.TestErrors.getValidationError}> Validation</Button>
+            </ButtonGroup>
+        </Container>
+    );
 }
