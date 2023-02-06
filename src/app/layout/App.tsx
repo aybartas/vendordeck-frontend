e1d32cb0c42712cc37300338a4c3b682ebef66d7
TUNATAS\aybar <aybartass@gmail.com>
2023-02-06 04:01:47 +0300

    react router v6 upgraded

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -2,23 +2,23 @@ import { Container, CssBaseline, CircularProgress } from '@mui/material';
 import Header from "./Header";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useEffect, useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
-import { ContactPage } from "@mui/icons-material";
-import AboutPage from "../features/about/AboutPage";
-import BasketPage from "../features/basket/BasketPage";
-import Catalog from "../features/catalog/Catalog";
-import ProductDetail from "../features/catalog/ProductDetail";
-import HomePage from "../features/home/HomePage";
 import { useStoreContext } from '../context/Context';
 import { getCookie } from "../utils/cookiesUtils";
 import { apiAgent } from '../api/ApiService';
+import HomePage from '../features/home/HomePage';
+import Catalog from '../features/catalog/Catalog';
+import ProductDetail from '../features/catalog/ProductDetail';
+import AboutPage from '../features/about/AboutPage';
+import ContactPage from '../features/contact/ContactPage';
+import BasketPage from '../features/basket/BasketPage';
 import CheckoutPage from '../features/checkout/CheckoutPage';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const [loading,setLoading] = useState(false);
-  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(false);
+  const { setBasket } = useStoreContext();
 
   const paletteSelection = darkMode ? "dark" : "light";
   const theme = createTheme({
@@ -29,43 +29,42 @@ function App() {
       },
     },
   });
-  
-  // use setBasket as dependency so the useEffect hook will be triggered only after setting basket
+
   useEffect(() => {
     const buyerId = getCookie('buyerId');
-    if(buyerId){
+    if (buyerId) {
       apiAgent.Basket.get()
-      .then(basket => setBasket(basket))
-      .catch(error => console.log(error))
-      .finally(() => setLoading(false));
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
     }
-    else{
+    else {
       setLoading(false);
     }
 
-  },[setBasket]);
+  }, [setBasket]);
 
-  if(loading) return (<CircularProgress />);
+  if (loading) return (<CircularProgress />);
 
   function handleThemeChange() {
     setDarkMode(!darkMode);
   }
-  console.log("app will render");
   return (
     <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-        <Container>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/catalog" component={Catalog} />
-            <Route path="/catalog/:id" component={ProductDetail} />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/contact" component={ContactPage} />
-            <Route path="/basket" component={BasketPage} />
-            <Route path="/checkout" component={CheckoutPage} />            
-          </Switch>
-        </Container>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:id" element={<ProductDetail />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/basket" element={<BasketPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+        </Routes>
+
+      </Container>
     </ThemeProvider>
   );
 }
